b453244a3c6a1e03ba1c787dab4cf9c44a78f843
Tanya933 <tanya.rajpal0400@gmail.com>
2022-03-02 13:58:45 +0530

    minor changes

--- index.js
+++ index.js
@@ -11,17 +11,17 @@ const app = express();
 const url = DB_URL;
 
 
-// app.use(
-//     cors({
-//         credentials: true,
-//         // origin:'*'
-//         // origin: 'http://localhost:3000'
-//         origin:'https://examination-helper-frontend.vercel.app'
-//  }));
+app.use(
+    cors({
+        credentials: true,
+        // origin:'*'
+        // origin: 'http://localhost:3000'
+        //origin:'https://examination-helper-frontend.vercel.app'
+ }));
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+// app.use(cors());
 
 app.get('/',(req,res)=>{
     res.send("welcome");
