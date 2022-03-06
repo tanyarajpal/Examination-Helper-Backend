afef097af96952ecd00aa01bf2f4c77fe5cb966a
Tanya933 <tanya.rajpal0400@gmail.com>
2022-03-07 01:46:45 +0530

    local storage token

--- index.js
+++ index.js
@@ -34,6 +34,8 @@ app.use((req, res, next) => {
     res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.append('Access-Control-Allow-Headers', 'Content-Type');
     res.append('Access-Control-Allow-Credentials', 'true');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-Access-Token");
+
     next();
 });
 
