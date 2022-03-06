d3a026a38ca6b7ad90222cf16cda4b5e78fb366e
Tanya933 <tanya.rajpal0400@gmail.com>
2022-03-06 22:02:17 +0530

    fixing cookies

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ app.use(cookieParser());
 app.use(express.json());
 
 
-app.use(
-    cors({
+// app.use(
+//     cors({
         
-        // origin:'*',
-        credentials: true,
-        // origin: 'http://localhost:3000'
-    //   origin:'https://examination-helper-frontend.vercel.app'
- }));
+//         // origin:'*',
+//         credentials: true,
+//         // origin: 'http://localhost:3000'
+//     //   origin:'https://examination-helper-frontend.vercel.app'
+//  }));
 // app.use(cors());
 
 app.get('/',(req,res)=>{
